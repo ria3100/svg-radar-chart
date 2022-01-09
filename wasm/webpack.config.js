ab02e46fffd1a2ff21a0d77079f256384203ac05
Ria <ria000130@gmail.com>
2022-01-09 16:37:15 +0900

    fix: build

---
+++ wasm/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: './index.ts',
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, `./lib/`),
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".wasm"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "esbuild-loader",
+        options: {
+          loader: 'ts',
+          target: 'es2015',
+          tsconfigRaw: require('./tsconfig.json')
+        }
+      }
+    ]
+  },
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true
+  }
+};
