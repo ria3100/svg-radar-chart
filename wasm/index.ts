345428ece221de9488ff2b5fc44f6fb4d456a9e2
Ria <ria000130@gmail.com>
2022-01-09 12:23:20 +0900

    feat: js側実装

--- wasm/index.ts
+++ wasm/index.ts
@@ -1,3 +1,20 @@
-export const foo = (x: string): string => {
-  return `foo ${x}`;
+import {radar, get_point} from './pkg/wasm.js';
+
+interface Rader {
+  print: string;
+  getPoint: (scale: number) => string;
+}
+
+export const rader = (score: [number], animate = false): Rader => {
+  const scale = animate ? 1 : 0;
+  const print = radar(new Int32Array(score), scale);
+
+  const getPoint = (scale: number) => {
+    return get_point(new Int32Array(score), scale);
+  };
+
+  return {
+    print,
+    getPoint,
+  };
 };
